1e25cfb01acaef17fe943011d86351d34c7cfe94
Midova <darya.demidova2011@gmail.com>
2016-09-29 12:43:45 +0300

    Интерфейс пользователя закончен. Привязала список операций

---
+++ MoneyAccounting2.0/MoneyAccounting/Example.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TransactionLibrary;
+
+namespace MoneyAccounting
+{
+	public class Example
+	{
+		public static Purse ComposePurse()
+		{
+			var result = new Purse("Example");
+			
+			result.MadeTransaction.Add(new TransactionMade(3, "категория1"));
+			result.MadeTransaction.Add(new TransactionMade(5, "категория2"));
+
+			return result;
+		}
+	}
+}
--- MoneyAccounting2.0/MoneyAccounting/MoneyAccountingViewModel.cs
+++ MoneyAccounting2.0/MoneyAccounting/MoneyAccountingViewModel.cs
@@ -6,7 +6,9 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Input;
 using TransactionLibrary;
-
+using Catel.MVVM;
+using System.Collections.ObjectModel;
+using System.Windows.Data;
 
 namespace MoneyAccounting
 {
@@ -18,26 +20,74 @@ namespace MoneyAccounting
 			_FileOpenDialogService = fileOpenDialogService;
 			_FileSaveDialogService = fileSaveDialogService;
 
-			//загрузки из файла.
-			LoadPurseCommand = new Command(LoadPurse);//подключить Catel. MVVM
+			//создаем и заполняем тестовый кошелк
+			_Purse = Example.ComposePurse();
 
-			//сохранение в файл.
+			//в приватное поле передаем список транзакций из "кошелька"
+			_TransactionMade = _Purse.MadeTransaction;
+
+			//создаем оболочку для работы со списком транзакций
+			ItemsTransactionMade = new ListCollectionView(_TransactionMade);
+			
+			
+			
+			//загрузки из файла.
+			LoadPurseCommand = new Command(LoadPurse);
+			
+						
 			
 		}
 
+		#region Infrastructure
+		
+		/// <summary>
+		/// поле: открытие файла
+		/// </summary>
 		private readonly IOpenProjectFileService _FileOpenDialogService;
-
+		
+		/// <summary>
+		/// поле: сохранение файла
+		/// </summary>
 		private readonly ISaveProjectFileService _FileSaveDialogService;
-
-		//поле кошелек
+		
+		/// <summary>
+		/// поле: кошелек
+		/// </summary>
 		private Purse _Purse;
+		
+		/// <summary>
+		/// поле: список операций
+		/// </summary>
+		private ObservableCollection<TransactionMade> _TransactionMade;
 
+		/// <summary>
+		/// Свойство: получает список соверешных транзакций. Это оболочка для работы со списком операций.
+		/// </summary>
+		public ListCollectionView ItemsTransactionMade { get; private set; }
+
+		#endregion
+
+		#region Working with Transaction
+
+
+		#endregion
+		
+		#region Load\Save Project
+
+		/// <summary>
+		/// Получает обработчик загрузки данных из файла
+		/// </summary>
 		public ICommand LoadPurseCommand { get; private set; }
 
+		/// <summary>
+		/// Метод загрузки данных из файла
+		/// </summary>
 		private void LoadPurse()
 		{
 
 		}
+
+		#endregion
 
 	}
 }
